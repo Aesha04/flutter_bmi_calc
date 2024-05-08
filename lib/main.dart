cb3570340a06dcdc31c5b8f4dac13ef5860e5786
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-05-08 22:32:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,379 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'bmipage.dart';
- 
-void main() => runApp(MyApp());
- 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown]);
     return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: HomePage(),
-      // routes: <String, WidgetBuilder>{
-      //   '/': (context) => HomePage(),
-      //   '/second': (context) => SecondApp()
-      // },
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: InputPage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  
+class InputPage extends StatefulWidget {
+  InputPage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  _InputPageState createState() => _InputPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  // final _textControler = new TextEditingController();
-
-  // @override
-  // void dispose() {
-  //   _textControler.dispose();
-  //   super.dispose();
-  // }
+class _InputPageState extends State<InputPage> {
+  int height = 170;
+  int weight = 70;
+  int age = 25;
+  Gender selectedGender = Gender.male;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BmiPage()
-      );
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    color: selectedGender == Gender.male
+                        ? Colors.blue
+                        : Colors.grey[300],
+                    cardChild: const IconContent(
+                      icon: Icons.male,
+                      label: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    color: selectedGender == Gender.female
+                        ? Colors.blue
+                        : Colors.grey[300],
+                    cardChild: const IconContent(
+                      icon: Icons.female,
+                      label: 'FEMALE',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              color: Colors.grey[300],
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('HEIGHT'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: const TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      const Text('cm'),
+                    ],
+                  ),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: Colors.grey[300],
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('WEIGHT'),
+                        Text(
+                          weight.toString(),
+                          style: const TextStyle(
+                            fontSize: 50.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (weight > 0) {
+                                    weight--;
+                                  }
+                                });
+                              },
+                              child: const Icon(Icons.remove),
+                            ),
+                            const SizedBox(width: 10.0),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              child: const Icon(Icons.add),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: Colors.grey[300],
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('AGE'),
+                        Text(
+                          age.toString(),
+                          style: const TextStyle(
+                            fontSize: 50.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (age > 0) {
+                                    age--;
+                                  }
+                                });
+                              },
+                              child: const Icon(Icons.remove),
+                            ),
+                            const SizedBox(width: 10.0),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              child: const Icon(Icons.add),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            height: 80.0,
+            child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResultsPage(
+                      height: height,
+                      weight: weight,
+                      age: age,
+                      gender: selectedGender,
+                    ),
+                  ),
+                );
+              },
+              child: const Text(
+                'CALCULATE BMI',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ResultsPage extends StatelessWidget {
+  final int height;
+  final int weight;
+  final int age;
+  final Gender gender;
+
+  ResultsPage({
+    Key? key,
+    required this.height,
+    required this.weight,
+    required this.age,
+    required this.gender,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double bmi = weight / ((height / 100) * (height / 100));
+    String bmiText = bmi.toStringAsFixed(1);
+    String interpretation;
+
+    if (bmi < 18.5) {
+      interpretation =
+          'You are UNDERWEIGHT. Try to gain weight by eating a balanced diet with plenty of protein and healthy fats. Consider consulting a dietitian.';
+    } else if (bmi >= 18.5 && bmi < 25) {
+      interpretation =
+          'You are at a NORMAL weight. Maintain your healthy weight by continuing to eat a balanced diet and staying physically active.';
+    } else if (bmi >= 25 && bmi < 30) {
+      interpretation =
+          'You are OVERWEIGHT. Focus on losing weight gradually by reducing calorie intake and increasing physical activity. Consult a healthcare professional for personalized advice.';
+    } else {
+      interpretation =
+          'You are OBESE. Take steps to improve your health by making changes to your diet and lifestyle. Consider seeking support from a healthcare professional or weight loss program.';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Results'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Normal BMI range:',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              '18.5 - 25 kg/m²',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 150.0),
+            const Text(
+              'Your BMI is:',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              bmiText,
+              style: const TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              interpretation,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+enum Gender { male, female }
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({super.key, required this.color, this.cardChild, this.onPress});
+
+  final Color? color;
+  final Widget? cardChild;
+  final Function? onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress as void Function()?,
+      child: Container(
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color!,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
+      ),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  const IconContent({super.key, required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey[700],
+          ),
+        ),
+      ],
+    );
   }
 }
